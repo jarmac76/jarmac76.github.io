0c5ce8a7301c7f8792b5bc34a8cf502d67365671
Jared McReynolds <mcreynoldsjared@gmail.com>
2024-03-29 15:33:01 -0400

    Spelling correction in Skills

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/Skills" element={<Skills />} />
           <Route path="/resume" element={<Resume />} />
